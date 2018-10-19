82c86bbf45db757a9c35b754d79a402fd441dcb8
miqrogroove <miqrogroove@gmail.com>
2018-10-19 18:22:23 -0400

    Add context menu item to dirs as well.

--- js/menu.js
+++ js/menu.js
@@ -4,16 +4,33 @@ function textviewerGo(file, data) {
         }, {escape: false});
 }
 
+function textviewerDir(file, data) {
+	var dir = data.dir;
+	if ( dir.length > 1 ) {
+		dir += '/';
+	}
+	window.location = OC.generateUrl('apps/files_textviewer{file}', {
+            'file': dir + file + '/'
+        }, {escape: false});
+}
+
 function textviewerHooks() {
 	OCA.Files.fileActions.registerAction({
 		name: 'viewtext',
-		displayName: 'View',
+		displayName: 'View Text',
 		mime: 'text',
 		permissions: OC.PERMISSION_READ,
 		icon: OC.imagePath('files_textviewer', 'eye'), //Icon made by Freepik from www.flaticon.com
 		actionHandler: textviewerGo
 	});
-	//OCA.Files.fileActions.setDefault('text', 'viewtext');
+	OCA.Files.fileActions.registerAction({
+		name: 'viewtext',
+		displayName: 'View Text',
+		mime: 'dir',
+		permissions: OC.PERMISSION_READ,
+		icon: OC.imagePath('files_textviewer', 'eye'),
+		actionHandler: textviewerDir
+	});
 }
 
 $(document).ready(function () {
